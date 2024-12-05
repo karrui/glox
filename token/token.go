edaeb515e91a3a7213dcbe3161033f57b98dab74
Kar Rui Lau <karrui@open.gov.sg>
2024-12-05 11:16:51 +0800

    feat(lexer): add support for keywords

--- token/token.go
+++ token/token.go
@@ -39,11 +39,21 @@ const (
 	// Keywords
 	FUNCTION = "FUNCTION"
 	LET      = "LET"
+	TRUE     = "TRUE"
+	FALSE    = "FALSE"
+	IF       = "IF"
+	ELSE     = "ELSE"
+	RETURN   = "RETURN"
 )
 
 var keywords = map[string]TokenType{
-	"fn":  FUNCTION,
-	"let": LET,
+	"fn":     FUNCTION,
+	"let":    LET,
+	"true":   TRUE,
+	"false":  FALSE,
+	"if":     IF,
+	"else":   ELSE,
+	"return": RETURN,
 }
 
 // Checks `keywords` table to see whether the given identifier is a keyword.
