4cd68d1325fa60c24dc08265818451a4286d7914
Kar Rui Lau <karrui@open.gov.sg>
2024-12-26 10:52:53 +0800

    feat: add parser ability to parse let statements
    page 41

--- parser/parser.go
+++ parser/parser.go
@@ -29,5 +29,75 @@ func (p *Parser) nextToken() {
 }
 
 func (p *Parser) ParseProgram() *ast.Program {
-	return nil
+	// :=& syntax
+	// program points to an initialized ast.Program with value {}
+	program := &ast.Program{}
+	// initialize Statements to array of ast.Statement structs
+	program.Statements = []ast.Statement{}
+
+	for p.curToken.Type != token.EOF {
+		// := syntax
+		// https://go.dev/ref/spec#Short_variable_declarations
+		// Shorthand for regular variable declaration with initializer expressions but no types
+		// Regular variable declaration: `var x = 5`
+		// Difference is that short variable declaration may redeclare variables provided
+		// originally declared in the same block with the same type.
+		// Redeclaration does not introduce a new variable; just new value to the original. E.g.
+		// 		field1, offset := nextField(str, 0)
+		//    field2, offset := nextField(str, offset)  // redeclares offset
+		//    x, y, x := 1, 2, 3                        // illegal: x repeated on left side of :=
+		stmt := p.parseStatement()
+		if stmt != nil {
+			program.Statements = append(program.Statements, stmt)
+		}
+		p.nextToken()
+	}
+
+	return program
+}
+
+func (p *Parser) parseStatement() ast.Statement {
+	switch p.curToken.Type {
+	case token.LET:
+		return p.parseLetStatement()
+	default:
+		return nil
+	}
+}
+
+func (p *Parser) parseLetStatement() *ast.LetStatement {
+	stmt := &ast.LetStatement{Token: p.curToken}
+
+	if !p.expectPeek(token.IDENT) {
+		return nil
+	}
+
+	stmt.Name = &ast.Identifier{Token: p.curToken, Value: p.curToken.Literal}
+
+	if !p.expectPeek(token.ASSIGN) {
+		return nil
+	}
+
+	// TODO: We're skipping the expressions until we encounter a semicolon
+	for !p.curTokenIs(token.SEMICOLON) {
+		p.nextToken()
+	}
+	return stmt
+}
+
+func (p *Parser) curTokenIs(t token.TokenType) bool {
+	return p.curToken.Type == t
+}
+
+func (p *Parser) peekTokenIs(t token.TokenType) bool {
+	return p.peekToken.Type == t
+}
+
+func (p *Parser) expectPeek(t token.TokenType) bool {
+	if p.peekTokenIs(t) {
+		p.nextToken()
+		return true
+	} else {
+		return false
+	}
 }
